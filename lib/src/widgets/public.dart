b65adf1ff78fe80bd5f9821745e182fb540e4897
Jtplouffe <jtplouffe@gmail.com>
2022-04-18 21:29:33 -0400

    Added missing export for ResponsiveDecoratedBox

--- lib/src/widgets/public.dart
+++ lib/src/widgets/public.dart
@@ -1,5 +1,6 @@
 export './base_responsive_widget.dart';
 export './responsive_breakpoint_provider.dart';
+export './responsive_decorated_box.dart';
 export './responsive_flex.dart';
 export './responsive_flexible.dart';
 export './responsive_padding.dart';
